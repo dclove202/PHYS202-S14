8ad2edc1f4d93e5e44eb312396eb988191e31f51
Dylan Love <dclove@csm-180-272-03.ad.calpoly.edu>
2014-04-03 15:36:17 -0700

    add ConfigureGit

---
+++ ConfigureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Dylan Love"
+git config --global user.email "dclove@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
